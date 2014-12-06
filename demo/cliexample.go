bf60bd9cb650c3d43890c3696d377b504479b118
Simon Waldherr <git@simon.waldherr.eu>
2014-12-06 23:33:41 +0100

    travis and coveralls

--- demo/cliexample.go
+++ demo/cliexample.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	bbmandelbrot ".."
 	"flag"
 	"fmt"
+	bbmandelbrot "github.com/SimonWaldherr/bbmandelbrot.go"
 	"image/png"
 	"os"
 	"runtime"
--- demo/webexample.go
+++ demo/webexample.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	bbmandelbrot ".."
 	"encoding/base64"
 	"fmt"
+	bbmandelbrot "github.com/SimonWaldherr/bbmandelbrot.go"
 	"image/png"
 	"io/ioutil"
 	"log"
